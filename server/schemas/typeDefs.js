064cd4c9d1e08181fe04d67b24a5d7eedb5541c5
ajmarrocco <ajmarrocco@gmail.com>
2022-08-10 16:29:42 -0400

    add schemas folder

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,10 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+// tagged template function from apollo 
+// advanced use of template literals
+const typeDefs = gql``;
+
+// export the typeDefs
+module.exports = typeDefs;
