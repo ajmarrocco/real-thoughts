189f4bcb00810afac9652b797c41468f246d8f7a
ajmarrocco <ajmarrocco@gmail.com>
2022-08-10 16:04:57 -0400

    add commnets in connection.js and server.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -6,5 +6,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/real-thoughts',
   useCreateIndex: true,
   useFindAndModify: false
 });
-
+// handles connection to the data base
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+// imports connection to data base
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Listens for connection 
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
